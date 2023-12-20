2552a1d6f06b51d019aad522217a81157e5f0884
jjjwww1024 <jjjwww1024@gmail.com>
2023-12-20 09:21:30 +0800

    first commit

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,6 +3,6 @@ import { defineUserConfig } from 'vuepress'
 export default defineUserConfig({
   lang: 'zh-CN',
   title: '你好， VuePress ！',
-  description: '这是我的第一个 VuePress 站点',
+  description: '这是第一个 VuePress 站点',
   base: '/',
 })
